59b20012ec4dca3a56c408560435c33cf51e4bf5
Tulshiram Panchal <76608808+TulshiramPanchal@users.noreply.github.com>
2025-06-28 18:03:46 +0530

    Create variable.tf

---
+++ azureCostOptimization/terraform/variable.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {}
+variable "location" {
+  default = "East US"
+}
+variable "cosmos_account_name" {}
+variable "database_name" {}
+variable "container_name" {}
+variable "storage_account_name" {}
